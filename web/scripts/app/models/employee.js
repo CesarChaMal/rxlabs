b6c70e6949d22e3054237ba098061e23f8457574
acnesiac <acnesiac@hotmail.com>
2016-10-29 16:39:15 -0700

    q q !q
        ~
    
        !
        q!
        q!

---
+++ web/scripts/app/models/employee.js
@@ -0,0 +1,20 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'app/collections/employees'
+], function($, _, Backbone, EmployeeCollection) {
+
+var Employee = Backbone.Model.extend({
+
+    urlRoot:"../api/employees",
+
+    initialize:function () {
+        //this.reports = new EmployeeCollection();
+        //this.reports.url = '../api/employees/' + this.id + '/reports';
+    }
+
+});
+
+  return Employee;
+});
---
+++ web/scripts/app/views/header.js
@@ -0,0 +1,49 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'app/collections/employees',
+  'app/views/employeelist',
+  'app/template',
+], function($, _, Backbone,  EmployeeCollection, EmployeeListView, Templates ) {
+
+  var HeaderView = Backbone.View.extend({
+    template: Templates['header'],
+    initialize: function () {
+        this.searchResults = new EmployeeCollection();
+        this.searchresultsView = new EmployeeListView({model: this.searchResults, className: 'dropdown-menu'});
+    },
+
+    render: function () {
+        $(this.el).html(Templates['header']);
+        $('.navbar-search', this.el).append(this.searchresultsView.render().el);
+        return this;
+    },
+
+    events: {
+        "keyup .search-query": "search",
+        "keypress .search-query": "onkeypress"
+    },
+
+    search: function () {
+        var key = $('#searchText').val();
+        this.searchResults.findByName(key);
+        setTimeout(function () {
+            $('.dropdown').addClass('open');
+        });
+    },
+
+    onkeypress: function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
+    },
+
+    select: function(menuItem) {
+        $('.nav li').removeClass('active');
+        $('.' + menuItem).addClass('active');
+    }
+  });
+
+    return HeaderView;
+});
